54ed242b41cc77b717ad97a6886da3f6eda7c5c4
Martin McLaughlin <martinmclaughin42@outlook.com>
2017-02-28 20:06:09 +0000

    including new tests

---
+++ FitnesseTests/FitnesseTests.cs
@@ -0,0 +1,22 @@
+﻿using Insurance;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using fit;
+
+namespace FitnesseTests
+{
+    public class FitnesseTests : fit.ColumnFixture
+    {
+        public int age;
+        public string gender;
+        public float CalcPremium()
+        {
+            var sut = new Customer();
+            return sut.CalcPremium(age, gender);
+        }
+    }
+
+}
